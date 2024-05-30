4edacd09c7a1602ac15d1111325a83bc6a06cfd0
Erb3 <49862976+Erb3@users.noreply.github.com>
2024-05-30 14:35:40 +0200

    feat: add all api endpoints

--- api.go
+++ api.go
@@ -1,6 +1,9 @@
 package switchcraftgo
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 type PlayerCountsResponse struct {
 	Total  int `json:"total"`
@@ -48,16 +51,20 @@ func GetTps() (*TpsResponse, error) {
 type PlayTimeLeaderboard struct {
 	UpdatedAt *time.Time
 	Entries   []struct {
+		Uuid     string `json:"uuid"`
 		Username string `json:"name"`
 		Seconds  int    `json:"time"`
 	}
 }
 
-// Fetches the leaderboard of who has been the most active player
+// Fetches the leaderboard of who has been the most active player.
+// Players are able to opt-out of this leaderboard.
+// Sorted in descending order.
 func GetPlaytimeLeaderboard() (*PlayTimeLeaderboard, error) {
 	var leaderboardData struct {
 		UpdatedAt string `json:"lastUpdated"`
 		Entries   []struct {
+			Uuid     string `json:"uuid"`
 			Username string `json:"name"`
 			Seconds  int    `json:"time"`
 		} `json:"entries"`
@@ -77,4 +84,91 @@ func GetPlaytimeLeaderboard() (*PlayTimeLeaderboard, error) {
 		UpdatedAt: &timestamp,
 		Entries:   leaderboardData.Entries,
 	}, nil
+}
+
+type SupporterGoal struct {
+	SupporterUrl string  `json:"supporterUrl"`
+	Current      float64 `json:"current"`
+	Goal         float64 `json:"goal"`
+	GoalMet      bool    `json:"goalMet"`
+}
+
+// Fetches information about the supporter goal
+//
+// Assuming success, the following information is returned:
+//   - SupporterUrl is a string containing the URL to support SwitchCraft
+//   - Current is a float of the current donated money this month, in dollars
+//   - Goal is a float of the supporter goal, in dollars
+//   - GoalMet reports whether the goal has been met
+//
+// In the case that something fails, the first return value is nil and the second is the error
+func GetSupporterGoal() (*SupporterGoal, error) {
+	var supporterData struct {
+		SupporterUrl string `json:"supporterUrl"`
+		Current      string `json:"current"`
+		Goal         string `json:"goal"`
+		GoalMet      bool   `json:"goalMet"`
+	}
+
+	err := makeGetJsonRequest("https://api.sc3.io/v3/supporter", &supporterData)
+	if err != nil {
+		return nil, err
+	}
+
+	current, err := strconv.ParseFloat(supporterData.Current, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	goal, err := strconv.ParseFloat(supporterData.Goal, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SupporterGoal{
+		SupporterUrl: supporterData.SupporterUrl,
+		Current:      current,
+		Goal:         goal,
+		GoalMet:      supporterData.GoalMet,
+	}, nil
+}
+
+type DeathsLeaderboard []struct {
+	Uuid  string `json:"uuid"`
+	Name  string `json:"name"`
+	Count int    `json:"count"`
+}
+
+// Fetches the leaderboard of most deaths.
+//
+// Returns an array of players along with the death count.
+// The array is sorted in descending order by most deaths.
+func GetDeathsLeaderboard() (DeathsLeaderboard, error) {
+	var leaderboard DeathsLeaderboard
+
+	err := makeGetJsonRequest("https://api.sc3.io/v3/deaths", &leaderboard)
+	if err != nil {
+		return nil, err
+	}
+
+	return leaderboard, nil
+}
+
+// Fetches the current ComputerCraft proxies
+//
+// Returns an array of IP ranges in CIDR notation that HTTP requests
+// originate from when sent from ComputerCraft.
+// This information can be used to block any traffic not originating from inside SwitchCraft.
+// For an IPv4 address, a CIDR range of /32 means that the IP address will match exactly.
+func GetProxyRanges() ([]string, error) {
+	var proxyInfo struct {
+		Proxies []string `json:"httpProxies"`
+	}
+
+	err := makeGetJsonRequest("https://api.sc3.io/v3/proxies", &proxyInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return proxyInfo.Proxies, nil
 }
--- api_test.go
+++ api_test.go
@@ -1,6 +1,9 @@
 package switchcraftgo
 
 import (
+	"fmt"
+	"log"
+	"strings"
 	"testing"
 )
 
@@ -24,6 +27,15 @@ func TestGetPlayerCounts(t *testing.T) {
 	}
 }
 
+func ExampleGetPlayerCounts() {
+	counts, err := GetPlayerCounts()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("There are currently %d players online.", counts.Total)
+}
+
 func TestGetTps(t *testing.T) {
 	res, err := GetTps()
 
@@ -42,6 +54,15 @@ func TestGetTps(t *testing.T) {
 	if res.MillisecondsLastTick == 0 {
 		t.Fatalf("GetTps() returned last milliseconds of zero, less than zero, or nil (got %f)", res.MillisecondsLastTick)
 	}
+}
+
+func ExampleGetTps() {
+	tpsData, err := GetTps()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("Average TPS: %f", tpsData.AverageTps)
 }
 
 func TestGetPlaytimeLeaderboard(t *testing.T) {
@@ -63,4 +84,106 @@ func TestGetPlaytimeLeaderboard(t *testing.T) {
 	if entry.Seconds <= 0 {
 		t.Fatalf("GetPlaytimeLeaderboard() returned zero seconds, less than zero, or nil on first entry (got %d)", entry.Seconds)
 	}
+}
+
+func ExampleGetPlaytimeLeaderboard() {
+	leaderboard, err := GetPlaytimeLeaderboard()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for idx, player := range leaderboard.Entries {
+		log.Printf("#%d: %s with %.0f hours.", idx+1, player.Username, float64(player.Seconds/60/60))
+	}
+}
+
+func TestGetSupporterGoal(t *testing.T) {
+	res, err := GetSupporterGoal()
+
+	if err != nil {
+		t.Fatalf("GetSupporterGoal() returned error %s", err.Error())
+	}
+
+	if res.SupporterUrl == "" {
+		t.Fatalf("GetSupporterGoal() returned no supporter url")
+	}
+
+	if res.Goal <= 0 {
+		t.Fatalf("GetSupporterGoal() returned goal less than 1$ (got %f)", res.Goal)
+	}
+
+	if res.Current >= res.Goal != res.GoalMet {
+		t.Fatalf("GetSupporterGoal() returned wrong GoalMet field. Got %f current, and %f as a goal.", res.Current, res.Goal)
+	}
+}
+
+func ExampleGetSupporterGoal() {
+	goal, err := GetSupporterGoal()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if !goal.GoalMet {
+		fmt.Printf("The supporter goal has not been reached this month. Consider supporting! %f has been donated so far this month, out of the %f required to operate.", goal.Current, goal.Goal)
+	}
+}
+
+func TestGetDeathsLeaderboard(t *testing.T) {
+	deaths, err := GetDeathsLeaderboard()
+
+	if err != nil {
+		t.Fatalf("GetDeathsLeaderboard() returned error %s", err.Error())
+	}
+
+	if deaths[0].Name == "" {
+		t.Fatalf("GetDeathsLeaderboard() returned no name for first entry")
+	}
+
+	if deaths[0].Uuid == "" {
+		t.Fatalf("GetDeathsLeaderboard() returned no uuid for first entry")
+	}
+
+	if deaths[0].Count == 0 {
+		t.Fatalf("GetDeathsLeaderboard() returned 0 deaths for first entry")
+	}
+
+	last := -1
+	for _, player := range deaths {
+		if last < player.Count && last != -1 {
+			t.Fatalf("GetDeathsLeaderboard() did not return leaderboard in descending order")
+		}
+
+		last = player.Count
+	}
+}
+
+func ExampleGetDeathsLeaderboard() {
+	deaths, err := GetDeathsLeaderboard()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%s has most deaths, with %d deaths", deaths[0].Name, deaths[0].Count)
+}
+
+func TestGetProxyRanges(t *testing.T) {
+	proxies, err := GetProxyRanges()
+
+	if err != nil {
+		t.Fatalf("GetProxyRanges() returned error %s", err.Error())
+	}
+
+	if len(proxies) == 0 {
+		t.Fatalf("GetProxyRanges() received no proxies")
+	}
+}
+
+func ExampleGetProxyRanges() {
+	proxies, err := GetProxyRanges()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("Current proxies: %s", strings.Join(proxies, ", "))
 }
